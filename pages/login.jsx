5cd27709d78ec7daa0c90f089fcf633a33dcecd1
Carlos Fermino <brutosacf@gmail.com>
2024-05-20 17:59:45 -0300

    Criado componente image

---
+++ pages/login.jsx
@@ -0,0 +1,10 @@
+import ImageWithSpace from "../src/components/layout/ImageWithSpace"
+
+function LoginPage () {
+  return (
+    <ImageWithSpace>
+      Teste 1234
+    </ImageWithSpace>
+  )
+}
+export default LoginPage
