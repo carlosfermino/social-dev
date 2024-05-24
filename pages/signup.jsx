8fd1991638de5f7393d64df3a85d6c3b94666754
Carlos Fermino <brutosacf@gmail.com>
2024-05-24 18:00:58 -0300

    Adicionado Joi e validação no signup

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -34,7 +34,7 @@ function SignupPage () {
           <Input label="Nome" type="text" />
           <Input label="Sobrenome" type="text" />
           <Input label="Usuário" type="text" />
-          <Input label="Email ou usuário" type="email" />
+          <Input label="Email" type="email" />
           <Input label="Senha" type="password" />
           <Button>Entrar</Button>
         </Form>
