b9335361017b8bc4868ac8c97838771cdca23513
Carlos Fermino <brutosacf@gmail.com>
2024-05-30 22:37:26 -0300

    Código com o RefreshPosts: Carregar novas postagens

--- src/components/cards/CreatePost.jsx
+++ src/components/cards/CreatePost.jsx
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
+import { useForm } from 'react-hook-form'
+import { joiResolver } from '@hookform/resolvers/joi'
+import axios from 'axios'
+import { useSWRConfig } from 'swr' 
+
+import { createPostSchema } from '../../../modules/post/post.schema'
 
 import H4 from '../typography/H4'
-import TextArea from '../inputs/TextArea'
+import ControlledTextArea from '../inputs/ControlledTextArea'
 import Button from '../inputs/Button'
+
 
 const PostContainer = styled.div`
 background-color: ${props => props.theme.white};
@@ -36,16 +43,38 @@ const BottonText = styled.p`
 `
 
 function CreatePost ({username}) {
+  const { mutate } = useSWRConfig()
+  const { control, handleSubmit, formState: {isValid}, reset } = useForm({
+    resolver: joiResolver(createPostSchema),
+    mode: 'all'
+  })
+
+  const onSubmit = async (data) => {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, data)
+    if (response.status === 201) {
+      reset()
+      mutate(`${process.env.NEXT_PUBLIC_API_URL}/api/post`)
+    }
+  }
+
   return (
     <PostContainer>
       <H4><Title>No que você está pensando, @{username}?</Title></H4>
-      <TextContainer>
-        <TextArea placeholder='Digite sua mensagem' rows='4' type='text' />
-      </TextContainer>
-      <BottonContainer>
-        <BottonText>A sua mensagem será pública.</BottonText>
-        <Button>Enviar mensagem</Button>
-      </BottonContainer>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextContainer>
+          <ControlledTextArea 
+            placeholder='Digite sua mensagem' 
+            rows='4' type='text' 
+            control={control} 
+            name='text'
+            maxLength= '256'
+          />
+        </TextContainer>
+        <BottonContainer>
+          <BottonText>A sua mensagem será pública.</BottonText>
+          <Button disabled={!isValid}>Postar mensagem</Button>
+        </BottonContainer>
+      </form>
     </PostContainer>
   )
 }
